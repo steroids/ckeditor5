7804912bb0075c4d6adc96d650742d62ed2542ae
antonovaekaterina <antonova.ekaterina19@yandex.ru>
2021-04-12 21:17:27 +0700

    27.0.2-rc.1

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -23,12 +23,24 @@ import List from '@ckeditor/ckeditor5-list/src/list.js';
 import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import { Paragraph } from 'ckeditor5/src/paragraph';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {
 }
@@ -57,40 +69,72 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableCellProperties,
 	TableProperties,
-	TableToolbar
+	TableToolbar,
+	Underline,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	BlockQuote,
+	Image,
+	ImageCaption,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	SimpleUploadAdapter
 ];
 
 // Editor configuration.
+const colors = [
+	'rgb(97,189,109)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)',
+	'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)', 'rgb(0,168,133)',
+	'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)', 'rgb(40,50,78)', 'rgb(0,0,0)',
+	'rgb(247,218,100)', 'rgb(251,160,38)', 'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)',
+	'rgb(239,239,239)', 'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
+	'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)',
+];
+
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
-			'|',
 			'bold',
 			'italic',
-			'link',
+			'underline',
+			'strikethrough',
+			'subscript',
+			'superscript',
+			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
+			'|',
 			'bulletedList',
 			'numberedList',
-			'|',
-			'outdent',
 			'indent',
-			'|',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo',
-			'|',
+			'outdent',
 			'alignment',
-			'code',
 			'|',
-			'removeFormat',
-			'fontBackgroundColor',
-			'fontColor',
-			'fontSize',
-			'fontFamily'
+			'link',
+			'imageUpload',
+			'blockQuote',
+			'insertTable',
+			'|',
+			'undo',
+			'redo'
 		]
 	},
 	language: 'ru',
+	fontFamily: {
+		options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
+	},
+	fontSize: {
+		options: [8, 9, 10, 11, 12, 13, 14, 15, 16],
+	},
+	fontColor: {colors},
+	fontBackgroundColor: {colors},
+	alignment: {
+		options: ['left', 'right', 'center', 'justify'],
+	},
 	table: {
 		contentToolbar: [
 			'tableColumn',
